d4716c3379dde0ecb4e41af0f2a1f9563a8376f0
mr-mangoranda <habibmangoranda15@gmail.com>
2026-01-01 13:05:50 +0800

    ROTC

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -5,8 +5,8 @@ import "./globals.css";
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
-  title: "Lama Dev School Management Dashboard",
-  description: "Next.js School Management System",
+  title: "MSU-ROTC UNIT",
+  description: "Mindanao State University Reserve Officer Training Corps",
 };
 
 export default function RootLayout({
--- src/components/Menu.tsx
+++ src/components/Menu.tsx
@@ -1,3 +1,6 @@
+import Image from "next/image";
+import Link from "next/link";
+
 const menuItems = [
   {
     title: "MENU",
@@ -6,85 +9,71 @@ const menuItems = [
         icon: "/home.png",
         label: "Home",
         href: "/",
-        visible: ["admin", "teacher", "student", "parent"],
       },
       {
         icon: "/teacher.png",
-        label: "Teachers",
-        href: "/list/teachers",
-        visible: ["admin", "teacher"],
+        label: "Cadet Officers",
+        href: "/teachers",
       },
       {
         icon: "/student.png",
-        label: "Students",
-        href: "/list/students",
-        visible: ["admin", "teacher"],
+        label: "Cadets",
+        href: "/students",
       },
       {
         icon: "/parent.png",
-        label: "Parents",
-        href: "/list/parents",
-        visible: ["admin", "teacher"],
+        label: "Training Staff",
+        href: "/parents",
       },
       {
         icon: "/subject.png",
         label: "Subjects",
-        href: "/list/subjects",
-        visible: ["admin"],
+        href: "/subjects",
       },
       {
         icon: "/class.png",
         label: "Classes",
-        href: "/list/classes",
-        visible: ["admin", "teacher"],
+        href: "/classes",
       },
       {
         icon: "/lesson.png",
         label: "Lessons",
-        href: "/list/lessons",
-        visible: ["admin", "teacher"],
+        href: "/lessons",
       },
       {
         icon: "/exam.png",
         label: "Exams",
-        href: "/list/exams",
-        visible: ["admin", "teacher", "student", "parent"],
+        href: "/exams",
       },
       {
         icon: "/assignment.png",
         label: "Assignments",
-        href: "/list/assignments",
-        visible: ["admin", "teacher", "student", "parent"],
+        href: "assignments",
       },
       {
         icon: "/result.png",
         label: "Results",
-        href: "/list/results",
-        visible: ["admin", "teacher", "student", "parent"],
+        href: "/results",
       },
       {
         icon: "/attendance.png",
         label: "Attendance",
-        href: "/list/attendance",
-        visible: ["admin", "teacher", "student", "parent"],
+        href: "/attendance",
       },
       {
         icon: "/calendar.png",
         label: "Events",
-        href: "/list/events",
-        visible: ["admin", "teacher", "student", "parent"],
+        href: "/events",
       },
       {
         icon: "/message.png",
         label: "Messages",
-        href: "/list/messages",
-        visible: ["admin", "teacher", "student", "parent"],
+        href: "/messages",
       },
       {
         icon: "/announcement.png",
         label: "Announcements",
-        href: "/list/announcements",
-        visible: ["admin", "teacher", "student", "parent"],
+        href: "/announcements",
       },
     ],
   },
@@ -95,20 +84,37 @@ const menuItems = [
         icon: "/profile.png",
         label: "Profile",
         href: "/profile",
-        visible: ["admin", "teacher", "student", "parent"],
       },
       {
         icon: "/setting.png",
         label: "Settings",
         href: "/settings",
-        visible: ["admin", "teacher", "student", "parent"],
       },
       {
         icon: "/logout.png",
         label: "Logout",
         href: "/logout",
-        visible: ["admin", "teacher", "student", "parent"],
       },
     ],
   },
 ];
+
+const Menu = () => {
+  return (
+    <div className="mt-4 text-sm">
+      {menuItems.map(i => (
+        <div className="flex flex-col gap-2" key={i.title}>
+          <span className="hidden lg:block text-gray-400 font-light my-4">{i.title}</span>
+          {i.items.map(item => (
+            <Link href={item.href} key={item.label} className="flex items-center justify-center lg:justify-start gap-4 text-gray-500 py-2">
+              <Image src={item.icon} alt="" width={20} height={20} />
+              <span className="hidden lg:block">{item.label}</span>
+            </Link>
+          ))}
+        </div>
+      ))}
+    </div>
+  )
+}
+
+export default Menu
